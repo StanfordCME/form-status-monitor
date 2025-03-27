2e758e53d6f5be13f66ed0446d8d6d74187b7539
Shehryar Warraich <shehryarwarraich@Mac.home>
2025-03-27 15:21:14 -0400

    changed staus.js

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,9 +1,29 @@
-const fs = require("fs");
+const fetch = require("node-fetch");
+
 exports.handler = async function () {
-  const data = fs.readFileSync("status.json", "utf8");
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: data
-  };
+  try {
+    const response = await fetch("https://api.uptimerobot.com/v2/getMonitors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        api_key: "ur2692690-656fe82844ad7140e68896c1", // 
+        format: "json",
+      }),
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
+
